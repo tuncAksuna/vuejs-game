702d57b80ed0096fc4954a4a4a4573c339319ba4
tuncAksuna <aksuna.tunc@gmail.com>
2021-12-23 13:02:17 +0300

    codes improved-computed prop. used

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 new Vue({
     el: "#app",
     data: {
-        player_heal: 100,
-        monster_heal: 100,
+        player_health: 100,
+        monster_health: 100,
         game_is_on: false,
-        logs: []
+        logs: [],
+        attack_multiple: 15,
+        special_attack_multiple: 25,
+        heal_up_multiple: 20,
+        monster_attack_multiple: 15,
+        log_text: {
+            attack: "PLAYER ATTACK : ",
+            special_attack: "SPECIAL ATTACK : ",
+            monster_attack: "MONSTER ATTACK : ",
+            heal_up: "HEAL UP : ",
+            give_up: "PLAYER GAVE UP :("
+        },
+
     },
     methods: {
         start_game: function() {
             this.game_is_on = true;
         },
         attack: function() {
-            var point = Math.ceil(Math.random() * 10);
-            this.monster_heal = this.monster_heal - point;
-            this.addLog({ turn: "Player", text: "PLAYER ATTACK (" + point + ")" })
+            var point = Math.ceil(Math.random() * this.attack_multiple);
+            this.monster_health = this.monster_health - point;
+            this.addLog({ turn: "Player", text: this.log_text.attack + point })
 
             this.monster_attack();
         },
         monster_attack: function() {
-            var point = Math.ceil(Math.random() * 15);
-            this.addLog({ turn: "Monster", text: "MONSTER ATTACK (" + point + ")" })
-            this.player_heal = this.player_heal - point;
+            var point = Math.ceil(Math.random() * this.monster_attack_multiple);
+            this.addLog({ turn: "Monster", text: this.log_text.monster_attack + point })
+            this.player_health = this.player_health - point;
         },
         special_attack: function() {
-            var point = Math.ceil(Math.random() * 25);
-            this.monster_heal -= point
-            this.addLog({ turn: "Player", text: "SPECIAL ATTACK (" + point + ")" })
+            var point = Math.ceil(Math.random() * this.special_attack_multiple);
+            this.monster_health -= point
+            this.addLog({ turn: "Player", text: this.log_text.special_attack + point })
             this.monster_attack();
         },
         heal_up: function() {
-            var point = Math.ceil(Math.random() * 20);
-            this.player_heal += point
-            this.addLog({ turn: "Player", text: "HEALTH (" + point + ")" })
+            var point = Math.ceil(Math.random() * this.heal_up_multiple);
+            this.player_health += point
+            this.addLog({ turn: "Player", text: this.log_text.heal_up + point })
             this.monster_attack();
         },
         give_up: function() {
-            this.player_heal = 0;
-            this.addLog({ turn: "Player", text: "Player gave up !" })
+            this.player_health = 0;
+            this.addLog({ turn: "Player", text: this.log_text.give_up })
         },
         addLog: function(log) {
             this.logs.push(log)
@@ -46,28 +58,43 @@ new Vue({
         /**
          * to watch healthies 
          */
-        player_heal: function(value) {
+        player_health: function(value) {
             if (value <= 0) {
-                this.player_heal = 0;
+                this.player_health = 0;
                 if (confirm("You are dead. Do you want to try again ? ")) {
-                    this.player_heal = 100;
-                    this.monster_heal = 100;
+                    this.player_health = 100;
+                    this.monster_health = 100;
                     this.logs = []
                 }
             } else if (value >= 100) {
-                this.player_heal = 100;
+                this.player_health = 100;
             }
         },
-        monster_heal: function(value) {
+        monster_health: function(value) {
             if (value <= 0) {
-                this.monster_heal = 0;
+                this.monster_health = 0;
                 if (confirm("You are winner now. Do you want to try again ? ")) {
-                    this.player_heal = 100;
-                    this.monster_heal = 100;
+                    this.player_health = 100;
+                    this.monster_health = 100;
                     this.logs = []
                 }
             } else if (value >= 100) {
-                this.monster_heal = 100;
+                this.monster_health = 100;
+            }
+        }
+    },
+    computed: {
+        /**
+         * @returns style of player and monster heal for logs 
+         */
+        player_progress: function() {
+            return {
+                width: this.player_health + '%',
+            }
+        },
+        monster_progress: function() {
+            return {
+                width: this.monster_health + '%',
             }
         }
     }
